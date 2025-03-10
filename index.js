c823ad0df6e371a6ac038597d9a5cfa521df0b39
PaingSC <paingsoechit.job@gmail.com>
2025-03-11 02:14:03 +0700

    Temp saving

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent');
+const { reject } = require('superagent/lib/request-base');
 
 // Using callback hell
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
@@ -17,10 +19,45 @@ const superagent = require('superagent');
 // });
 
 // Using Promises
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((result) => {
+//       console.log(result.body.message);
+
+//       fs.writeFile('dog-img.txt', result.body.message, (err) => {
+//         console.log('Rondom dog image saved to file');
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
+
+// Building Promises
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 🥱😵😵‍💫');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write the file 🤯');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`).then((result) => {
+  console.log(`Breed: ${result}`);
   superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .get(`https://dog.ceo/api/breed/${result}/images/random`)
     .then((result) => {
       console.log(result.body.message);
 
